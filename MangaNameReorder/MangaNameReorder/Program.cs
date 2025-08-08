4d22934718294b8607aeb2008e89e32b8c470a13
neohind <neohind@gmail.com>
2025-08-08 20:56:53 +0900

    프로젝트 등록

---
+++ MangaNameReorder/MangaNameReorder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilenameReplace
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>     
+        static void Main()
+        {
+            
+        }
+    }
+}
